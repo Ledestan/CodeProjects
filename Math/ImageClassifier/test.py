5b27b75975c9ccc1f55bb413546a3bcb2e2ada3a
Tingchu <TingchuMailbox@163.com>
2026-05-08 08:30:29 +0800

    Updata.

--- Math/ImageClassifier/test.py
+++ Math/ImageClassifier/test.py
@@ -84,7 +84,7 @@ class Window:
 
 def main():
     # 加载模型参数
-    param_path = 'data/models/params.npz'
+    param_path = "data/models/params.npz"
     if not os.path.exists(param_path):
         root = tk.Tk()
         root.withdraw()
